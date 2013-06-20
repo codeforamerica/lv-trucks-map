baf6a29ed3a043e0ded4ef32b5130efe9e521d7c
Lou Huang <lou@codeforamerica.org>
2013-06-19 19:54:29 -0700

    Massive interface overhaul. (removed generic CLV header, color/font tweaks, etc.) Add in logic for pulling data and displaying trucks from external sources.

--- js/main.js
+++ js/main.js
@@ -1,98 +1,159 @@
+(function() { 
+  document.data = function() {
 
-$(document).ready(function() {
+    // LOAD SOME EXTERNAL DATAS
+    var dataJson = 'data.json',
+        data = []
 
-    // size #main block via jquery - kind of wonky right now
-/*    $('#main').each(function() {
-        var p = $(this).parent();
-        $(this).height($('body').height() - $('header').height() - $('footer').height());
-    });
-*/
+    $.ajax({
+        url: dataJson,
+        async: false,
+        dataType: 'json',
+        success: function (i) {
+            data = i
+        }
+    })
+
+    return {
+        locations: data.locations,
+        trucks: data.trucks,
+        calendar: data.calendar
+    }
+
+  }
+})();
+
+$(document).ready( function () {
+
     // TRUCK INFO - Dragger
-    $('#truck-info').draggable({ handle: '.truck-title', containment: '#main', cursor: '-webkit-grabbing !important'});
+    // $('#truck-info').draggable({ handle: '.truck-title', containment: '#main', cursor: '-webkit-grabbing !important'})
 
     // TRUCK HEADING - Hi-liter
     // Note on hiliters - consider using CSS3 transitions instead of jQuery?
     var truckHeadingColor = $('.truck-heading').css('background-color'),
-        truckHiliteColor = $('.truck-heading-hilite').css('background-color');
+        truckHiliteColor = $('.truck-heading-hilite').css('background-color')
 
     $('.truck-heading').hover( function () {
         $(this).animate({
             backgroundColor: truckHiliteColor,
-        }, 200 );
+        }, 200 )
     }, function () {
         $(this).animate({
             backgroundColor: truckHeadingColor,
-        }, 200 );
-    });
+        }, 200 )
+    })
 
     // TRUCK HEADING - toggler for entries
     $('.truck-heading').click( function () {
-        toggleTruckEntries($(this));
-    });
+        toggleTruckEntries($(this))
+    })
 
     // TRUCK ENTRY - Hi-liter
     var entryBgColor = $('.truck-entry').css('background-color'),
-        entryHiliteColor = $('.truck-entry-hilite').css('background-color');
+        entryHiliteColor = $('.truck-entry-hilite').css('background-color')
 
-    $('.truck-entry').hover( function () {
-        $(this).animate({
-            backgroundColor: entryHiliteColor,
-        }, 200 );
-    }, function () {
-        $(this).animate({
-            backgroundColor: entryBgColor,
-        }, 200 );
-    });
+    $('#truck-info').on('mouseenter', '.truck-entry', function () {
+        $(this).css('background-color', entryHiliteColor)
+    })
+    $('#truck-info').on('mouseleave', '.truck-entry', function () {
+        $(this).css('background-color', entryBgColor)
+    })
 
     // FOOTER POPUPS
-    $('#footer-trucks-link').click( function () {
-        toggleFooterPopup('#footer-trucks');
-    });
-    $('#footer-calendar-link').click( function () {
-        toggleFooterPopup('#footer-calendar');
-    });
-    $('#footer-mfv-link').click( function () {
-        toggleFooterPopup('#footer-mfv');
-    });
-    $('#footer-about-link').click( function () {
-        toggleFooterPopup('#footer-about');
-    });
-    // Click outside a popup box to close it
+    // Open / toggle
+    $('#footer-trucks-link').click( function () { toggleFooterPopup('#footer-trucks') })
+    $('#footer-calendar-link').click( function () { toggleFooterPopup('#footer-calendar') })
+    $('#footer-mfv-link').click( function () { toggleFooterPopup('#footer-mfv') })
+    $('#footer-about-link').click( function () { toggleFooterPopup('#footer-about') })
+    // Close popups
+    // -- when X is clicked on inside the popup
+    $('.footer-popup-close').click( function () {
+        $('.footer-popup').slideUp(200)
+    })
+    // -- when user clicks outside the popup
     $('#main').click( function () {
-        $('.footer-popup').slideUp(200);
-    });
+        $('.footer-popup').slideUp(200)
+    })
 
-});
 
+
+
+    // INITIALISE!
+    $('#truck-head-now').click()
+
+    displayTruckEntries(calendar.now, '#truck-info-now')
+    displayTruckEntries(calendar.later, '#truck-info-later')
+    displayTruckEntries(calendar.muchlater, '#truck-info-muchlater')
+
+    // Populate footer elements
+    var mFooterAllTrucks = $('#m-footer-all-trucks').html()
+    $('#footer-trucks .footer-popup-content').html(Mustache.render(mFooterAllTrucks, data))
+
+})
 
 function toggleTruckEntries(clickedHeading) {
-    var entries = clickedHeading.next('.truck-entries');
+    var entries = clickedHeading.next('.truck-entries')
     if ( entries.is(':visible') ) {
-        entries.slideUp(200);
-        clickedHeading.addClass('truck-heading-border');
-        clickedHeading.removeClass('active');
+        entries.slideUp(200)
+        clickedHeading.addClass('truck-heading-border')
+        clickedHeading.removeClass('active')
     } else {
-        entries.slideDown(200);
-        clickedHeading.removeClass('truck-heading-border');
-        clickedHeading.addClass('active');
+        entries.slideDown(200)
+        clickedHeading.removeClass('truck-heading-border')
+        clickedHeading.addClass('active')
     }
 }
 
 function toggleFooterPopup(target) {
     if ($(target).is(':visible')) {
-        $(target).slideUp(200);
+        $(target).slideUp(200)
     } else {
         // hide all others
-        $('.footer-popup').slideUp(200);
+        $('.footer-popup').slideUp(200)
 
         // get the position of the clicked link so that the popup box lines up
         // there may be a better way of doing this somehow, that doesn't rely on programatically determining position. what happens if a window resizes?
-        var link = target + '-link';
-        var position = $(link).offset().left;
-        $(target).css('left', position);
+        var link = target + '-link'
+        var position = $(link).offset().left
+        $(target).css('left', position)
 
         // display the popup
-        $(target).slideDown(200);
+        $(target).slideDown(200)
     }
 }
 
+function displayTruckEntries(calendar, section) {
+
+    var mTruckEntry = $('#m-truck-entry').html()
+    var trucksObject = {}
+    trucksObject.entries = []
+
+    for (i = 0; i < calendar.length; i++) {
+
+        var truckID = calendar[i].id
+        var locationID = calendar[i].at
+
+        for (j = 0; j < trucks.length; j++) {
+            if (trucks[j].id == truckID) {
+                trucksObject.entries[i] = trucks[j]
+            }
+        }
+
+        for (k = 0; k < locations.length; k++) {
+            if (locations[k].id == locationID) {
+                trucksObject.entries[i].location = locations[k].name
+                trucksObject.entries[i].address = locations[k].address
+            }
+        }
+
+        trucksObject.entries[i].date = calendar[i].date
+        trucksObject.entries[i].from = calendar[i].from
+        trucksObject.entries[i].until = calendar[i].until
+
+        if(!trucksObject.entries[i]) {
+            console.log('error: problem with truck entries.')
+        }
+    }
+
+    $(section).append(Mustache.render(mTruckEntry, trucksObject))
+}
--- js/map.js
+++ js/map.js
@@ -2,29 +2,51 @@
 // Initialize map
 //var map = L.mapbox.map('map', 'examples.map-4l7djmvo')
 var map = L.mapbox.map('map', 'examples.map-y7l23tes')
-    .setView([36.1665, -115.1479], 17);
+            .setView([36.1665, -115.1479], 17)
+/*
+var attrib = 'Map tiles by <a href="http://mapbox.com">MapBox</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
+
+var layer = L.mapbox.tileLayer('examples.map-y7l23tes', {
+    mapAttribution: attrib
+})
+*/
 
 // Set up truck icon
 var truckMarker = L.icon({
     iconUrl: 'img/marker-truck.svg',
 
-    iconSize:     [55, 30], // size of the icon
-    iconAnchor:   [27, 5], // point of the icon which will correspond to marker's location
+    iconSize:     [70, 60], // size of the icon
+    iconAnchor:   [35, 50], // point of the icon which will correspond to marker's location
     popupAnchor:  [0, -50] // point from which the popup should open relative to the iconAnchor
-});
+})
 
-// City hall location
-L.marker([36.167866, -115.147866], {
-    icon: truckMarker
-}).addTo(map);
+// Get data
+var data = document.data()
 
-// Bonneville Transit Center
-L.marker([36.164973, -115.148048], {
-    icon: truckMarker
-}).addTo(map);
+// This information is now also available back in main.js
+var locations = data.locations
+var trucks = data.trucks
+var calendar = data.calendar
 
-// RJC
-L.marker([36.166047, -115.145667], {
-    icon: truckMarker
-}).addTo(map);
+
+// Populate map with locations
+var locationGroup = new L.FeatureGroup()
+var locationMarker = []
+
+for (i = 0; i < locations.length; i++) {
+    var lat = locations[i].lat
+    var lng = locations[i].lng
+
+    locationMarker[i] = new L.Marker([lat, lng], {
+        icon: truckMarker
+    }).addTo(locationGroup)
+}
+
+locationGroup.addTo(map)
+
+// Set view based on locations
+// Not exactly necessary right now.
+map.fitBounds(locationGroup.getBounds())
+
+
 
